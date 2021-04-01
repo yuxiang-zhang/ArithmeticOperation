b194f7e7a3e5ff29e43486a3314b05c7a8194c87
yuxiang-zhang <yxz.get@gmail.com>
2021-04-01 16:37:16 -0400

    comment out testSub to test code coverage decrease

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -17,15 +17,16 @@ public class ArithmeticOperationsTest {
 		assertEquals(expected, actual);
 
 	}
-    @Test
-    public void testSub()
-    {
+	
+//     @Test
+//     public void testSub()
+//     {
 
-        ArithmeticOperations operations = new ArithmeticOperations();
-        Integer actual = operations.addOrSub(6, 2);
-        Integer expected = 4;
-        assertEquals(expected, actual);
+//         ArithmeticOperations operations = new ArithmeticOperations();
+//         Integer actual = operations.addOrSub(6, 2);
+//         Integer expected = 4;
+//         assertEquals(expected, actual);
 
-    }
+//     }
 
 }
