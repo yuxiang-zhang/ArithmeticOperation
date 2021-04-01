d24ef5757caffb12c77c237441c1f74438258da3
yuxiang-zhang <yxz.get@gmail.com>
2021-04-01 17:01:32 -0400

    simulate test fail to test email notification

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -3,6 +3,7 @@ package soen.tutorial;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.Fail;
 
 public class ArithmeticOperationsTest {
 
@@ -15,6 +16,7 @@ public class ArithmeticOperationsTest {
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
 		assertEquals(expected, actual);
+		Fail("test fail");
 
 	}
 	
