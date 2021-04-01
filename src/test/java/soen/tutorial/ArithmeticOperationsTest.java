1aeb7377223f1c278bb8b2eae08172bd977f3eb2
yuxiang <yuxiang.zhang@mail.concordia.ca>
2021-04-01 17:20:39 -0400

    fix fail wrong capitalization

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -3,7 +3,7 @@ package soen.tutorial;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.Fail;
+import static org.junit.Assert.fail;
 
 public class ArithmeticOperationsTest {
 
@@ -16,7 +16,7 @@ public class ArithmeticOperationsTest {
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
 		assertEquals(expected, actual);
-		Fail("test fail");
+		fail("test fail");
 
 	}
 	
